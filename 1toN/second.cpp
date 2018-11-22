e69f0f0e0eaf3ecbd3f744745ee6b92a9685751c
myyrakle <sssang97@naver.com>
2018-11-22 23:33:38 +0900

    Add files via upload

--- 1toN/second.cpp
+++ 1toN/second.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 
+#include <list>
+
 //버블소트가 너무 느려서 내장 정렬함수인 std::sort 사용.
+//빈 공간의 유연한 처리를 위해, 배열 대신 list 사용
 
 //std::sort는 퀵소트, 힙소트, 삽입소트의 혼합이다.
 //std::sort는 기본적으로 퀵소트를 쓰다가, 퀵소트가 너무 느린 경우라면 힙소트를 쓴다.
@@ -25,17 +28,12 @@ long long sort_time = 0;
 	std::array<int, InterfaceSize> wating_array{ 0, }; //0으로 초기화
 
 	//인덱스용 배열
-	std::array<int*, InterfaceSize> indexing_array{ nullptr, };
+	std::list<int*> indexing_list;
 
 	//일단 초기화
-	int i = 0;
 	for (auto& arr : interface_array)
 		for (auto& e : arr)
-		{
-			indexing_array[i] = &e;
-			i++;
-		}
-
+			indexing_list.push_back(&e);
 
 	bool on_first = true;
 
@@ -65,7 +63,7 @@ long long sort_time = 0;
 			}
 
 			//최초 정렬
-			std::sort(indexing_array.begin(), indexing_array.end(),
+			indexing_list.sort(
 				[](int* lhs, int* rhs) {return (*lhs) < (*rhs); }
 			);
 
@@ -84,23 +82,20 @@ long long sort_time = 0;
 		//25번 반복해서 확인하고 넣고...
 		for (int i = 0; i < InterfaceSize; i++)
 		{
-			int zero_count = 0;
-			
 timer.start();
 			//현재 인터페이스에서 제일 작은걸 찾습니다. 물론 0 빼고요
-			int* min_pos = indexing_array[zero_count];
+			int* min_pos = indexing_list.front();
 timer.stop();
 search_time += timer.get_nano();
 			//빼고 대기중인 값 삽입
 
 			//std::cout << "최소값: " << *min_pos << std::endl << std::endl << std::endl;
 
-
 			//대기배열의 값이 MAX보다 큰 경우 0 삽입
 			if (wating_array[i] > MAX)
 			{
 				*min_pos = 0;
-				zero_count++;
+				indexing_list.pop_front();
 			}
 			//대기배열의 값이 MAX아래일 경우에만 인터페이스의 값 변경
 			else
@@ -109,7 +104,7 @@ search_time += timer.get_nano();
 
 			//저장했으니까 정렬
 timer.start();
-			std::sort(indexing_array.begin(), indexing_array.end(),
+			indexing_list.sort(
 				[](int* lhs, int* rhs)
 			{
 				return (*lhs) < (*rhs);
